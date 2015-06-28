cdc19305ff2b1e7d012cdeb6335c381abfdc7929
Brexton Pham <brextonpham@stanford.edu>
2015-06-28 14:36:25 -0400

    Implemented a SAMCache CocoaPod to cache photos.
    Prevents repeated downloads by saving/accessing photos locally

--- Photo Bombers/THPhotoCell.m
+++ Photo Bombers/THPhotoCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "THPhotoCell.h"
+#import <SAMCache/SAMCache.h>
 
 @implementation THPhotoCell
 
@@ -15,7 +16,7 @@
     _photo = photo;
     
     //download photo from instagram and puts it in imageview as well as set photo property
-    NSURL *url = [[NSURL alloc] initWithString:_photo[@"images"][@"standard_resolution"][@"url"]];
+    NSURL *url = [[NSURL alloc] initWithString:_photo[@"images"][@"thumbnail"][@"url"]];
     [self downloadPhotoWithURL:url];
 }
 
@@ -40,12 +41,22 @@
 }
 
 - (void)downloadPhotoWithURL:(NSURL *)url {
+    //check if photo is cached already, if so, return it
+    NSString *key = [[NSString alloc] initWithFormat:@"%@-thumbnail", self.photo[@"id"]];
+    UIImage *photo = [[SAMCache sharedCache] imageForKey:key];
+    if (photo) {
+        self.imageView.image = photo;
+        return;
+    }
+    
     NSURLSession *session = [NSURLSession sharedSession];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
     NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
         //get image from location and set it on imageview
         NSData *data = [[NSData alloc] initWithContentsOfURL:location];
-        UIImage *image = [[UIImage alloc] initWithData:data]; 
+        UIImage *image = [[UIImage alloc] initWithData:data];
+        [[SAMCache sharedCache] setImage:image forKey:key]; //save in cache!
+        
         dispatch_async(dispatch_get_main_queue(), ^{
             self.imageView.image = image;
         });
